1a2747776cbc1bcce1e0ce558817defb5ce47c90
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-08 07:00:53 -0300

    Empty spring boot app

---
+++ src/main/java/com/alejosilvalau/cloud/photos/app/CloudPhotosAppApplication.java
@@ -0,0 +1,13 @@
+package com.alejosilvalau.cloud.photos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPhotosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPhotosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejosilvalau/cloud/photos/app/CloudPhotosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejosilvalau.cloud.photos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPhotosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
