b3d76bb6a7ef31b88d3c7b59bc5ad507e90dd20d
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-08 08:30:54 -0300

    Added the first method to get records from the in-memory db

--- src/main/java/com/alejosilvalau/cloud/photos/app/PhotosController.java
+++ src/main/java/com/alejosilvalau/cloud/photos/app/PhotosController.java
@@ -1,13 +1,22 @@
 package com.alejosilvalau.cloud.photos.app;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PhotosController {
-	
-	@GetMapping("/")
-	public String hello() {
-		return "Hello World";
-	}
+  private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+
+  @GetMapping("/")
+  public String hello() {
+    return "Hello World";
+  }
+
+  @GetMapping("/photos")
+  public List<Photo> get() {
+    return db;
+  }
+
 }
