2e2fada6e5e677f21f657cf92734bc7794f0419f
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-09 17:39:54 -0300

    Added first POST method

--- src/main/java/com/alejosilvalau/cloud/photos/app/PhotosController.java
+++ src/main/java/com/alejosilvalau/cloud/photos/app/PhotosController.java
@@ -3,11 +3,14 @@ package com.alejosilvalau.cloud.photos.app;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -40,5 +43,12 @@ public class PhotosController {
   public void delete(@PathVariable String id) {
     Photo photo = db.remove(id);
     if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+  }
+  
+  @PostMapping("/photos")
+  public Photo create(@RequestBody Photo photo) {
+    photo.setId(UUID.randomUUID().toString());
+    db.put(photo.getId(), photo);
+    return photo;
   }
 }
