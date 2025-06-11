a68835600285ecfa37b84e630cbadbcd5b765fa4
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-11 19:05:42 -0300

    Changed the place of the App Application file

--- src/main/java/com/alejosilvalau/cloud/photos/app/CloudPhotosAppApplication.java
+++ src/main/java/com/alejosilvalau/cloud/photos/app/CloudPhotosAppApplication.java
@@ -1,4 +1,4 @@
-package com.alejosilvalau.cloud.photos.app.Photos;
+package com.alejosilvalau.cloud.photos.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
