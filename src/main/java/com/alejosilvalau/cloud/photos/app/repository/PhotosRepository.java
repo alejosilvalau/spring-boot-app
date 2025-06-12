51e576314ea9235bcaf431c5e777363bf3f5e0d5
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-12 11:27:19 -0300

    Implemented an H2 SQL table and a repository for CRUD operations

---
+++ src/main/java/com/alejosilvalau/cloud/photos/app/repository/PhotosRepository.java
@@ -0,0 +1,7 @@
+package com.alejosilvalau.cloud.photos.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.alejosilvalau.cloud.photos.app.model.Photo;
+
+public interface PhotosRepository extends CrudRepository<Integer, Photo> {}
